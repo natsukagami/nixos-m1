a06a673c9c01ed4146e7536fcb9f2d14fe87a05a
Natsu Kagami <nki@nkagami.me>
2022-12-07 18:29:56 +0100

    Use new rev

--- nix/m1-support/kernel/package.nix
+++ nix/m1-support/kernel/package.nix
@@ -1,4 +1,5 @@
-{ pkgs, _4KBuild ? false, kernelPatches ? [ ] }: let
+{ pkgs, _4KBuild ? false, kernelPatches ? [ ] }:
+let
   localPkgs =
     # we do this so the config can be read on any system and not affect
     # the output hash
@@ -7,13 +8,17 @@
 
   lib = localPkgs.lib;
 
-  parseExtraConfig = cfg: let
-    lines = builtins.filter (s: s != "") (lib.strings.splitString "\n" cfg);
-    perLine = line: let
-      kv = lib.strings.splitString " " line;
-    in assert (builtins.length kv == 2);
-       "CONFIG_${builtins.elemAt kv 0}=${builtins.elemAt kv 1}";
-    in lib.strings.concatMapStringsSep "\n" perLine lines;
+  parseExtraConfig = cfg:
+    let
+      lines = builtins.filter (s: s != "") (lib.strings.splitString "\n" cfg);
+      perLine = line:
+        let
+          kv = lib.strings.splitString " " line;
+        in
+        assert (builtins.length kv == 2);
+        "CONFIG_${builtins.elemAt kv 0}=${builtins.elemAt kv 1}";
+    in
+    lib.strings.concatMapStringsSep "\n" perLine lines;
 
   readConfig = configfile: import (localPkgs.runCommand "config.nix" { } ''
     echo "{ } // " > "$out"
@@ -37,43 +42,47 @@
 
       _kernelPatches = kernelPatches;
     in
-    linuxKernel.manualConfig rec {
-      inherit stdenv lib;
+    linuxKernel.manualConfig
+      rec {
+        inherit stdenv lib;
 
-      version = "6.1.0-rc8-asahi";
-      modDirVersion = version;
+        version = "6.1.0-rc8-asahi";
+        modDirVersion = version;
 
-      src = fetchFromGitHub {
-        # tracking: https://github.com/AsahiLinux/PKGBUILDs/blob/stable/linux-asahi/PKGBUILD
-        owner = "AsahiLinux";
-        repo = "linux";
-        rev = "asahi-6.1-rc8-2";
-        hash = "sha256-P4PiqD4tF8ZiOxY59O4mYhDuQMZkoMjJuqmRGN0hJ/o=";
-      };
+        src = fetchFromGitHub {
+          # tracking: https://github.com/AsahiLinux/PKGBUILDs/blob/stable/linux-asahi/PKGBUILD
+          owner = "AsahiLinux";
+          repo = "linux";
+          rev = "asahi-6.1-rc8-3";
+          hash = "sha256-DIfYpxyF8rjrkyiR7qxVphBnezTZ3JF6GTspbtuIIhc=";
+        };
 
-      kernelPatches = [
-      ] ++ lib.optionals _4KBuild [
-        # thanks to Sven Peter
-        # https://lore.kernel.org/linux-iommu/20211019163737.46269-1-sven@svenpeter.dev/
-        { name = "sven-iommu-4k";
-          patch = ./sven-iommu-4k.patch;
-        }
-      ] ++ lib.optionals (!_4KBuild) [
-        # patch the kernel to set the default size to 16k instead of modifying
-        # the config so we don't need to convert our config to the nixos
-        # infrastructure or patch it and thus introduce a dependency on the host
-        # system architecture
-        { name = "default-pagesize-16k";
-          patch = ./default-pagesize-16k.patch;
-        }
-      ] ++ _kernelPatches;
+        kernelPatches = [
+        ] ++ lib.optionals _4KBuild [
+          # thanks to Sven Peter
+          # https://lore.kernel.org/linux-iommu/20211019163737.46269-1-sven@svenpeter.dev/
+          {
+            name = "sven-iommu-4k";
+            patch = ./sven-iommu-4k.patch;
+          }
+        ] ++ lib.optionals (!_4KBuild) [
+          # patch the kernel to set the default size to 16k instead of modifying
+          # the config so we don't need to convert our config to the nixos
+          # infrastructure or patch it and thus introduce a dependency on the host
+          # system architecture
+          {
+            name = "default-pagesize-16k";
+            patch = ./default-pagesize-16k.patch;
+          }
+        ] ++ _kernelPatches;
 
-      inherit configfile;
-      config = readConfig configfile;
+        inherit configfile;
+        config = readConfig configfile;
 
-      extraMeta.branch = "6.1";
-    } // (args.argsOverride or {});
+        extraMeta.branch = "6.1";
+      } // (args.argsOverride or { });
 
   linux_asahi = (pkgs.callPackage linux_asahi_pkg { });
-in pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_asahi)
+in
+pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux_asahi)
 
